9f85f038a58e249650493e2aed59662bdce30e7f
VikramKumarstm <vikramclg0000@gmail.com>
2025-12-03 15:17:20 +0530

    manage log using morgan

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
 
 const app = express();
 
@@ -13,6 +14,8 @@ app.use(cors({
 }));
 
 app.use(cookieParser());
+
+app.use(morgan('dev'))
 
 app.use('/ping', function(req, res) {
     res.send('/pong')
