99f799993f51477e16186e13c354a5c52c54fffa
VikramKumarstm <vikramclg0000@gmail.com>
2025-12-08 09:37:46 +0530

    upload image on cloudinary

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import AppError from "../utils/error.utils.js";
+import cloudinary from "cloudinary";
+import fs from 'fs/promises';
 
 const cookieOptions = {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
@@ -41,6 +43,33 @@ export const register = async (req, res, next) => {
         }
 
         //TODO: File upload
+        console.log('File Details > ', JSON.stringify(req.file));
+        if(req.file) {
+            
+            try {
+
+                const result = await cloudinary.v2.uploader.upload(req.file.path, {
+                    folder: 'lms',
+                    width: 250,
+                    height: 250,
+                    gravity: 'faces',
+                    crop: 'fill',
+                });
+
+                if(result) {
+                    user.avatar.public_id = result.public_id;
+                    user.avatar.secure_url = result.secure_url;
+
+                    //Remove file from server
+                    fs.rm(`uploads/${req.file.filename}`)
+                }
+                
+            } catch (error) {
+
+                return next(error || 'file not uploaded! please try again', 500)
+                
+            }
+        }
 
         await user.save();
 
