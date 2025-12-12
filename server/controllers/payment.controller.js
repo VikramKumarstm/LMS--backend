f73cff15f0c77765638cba2446f638f210989811
VikramKumarstm <vikramclg0000@gmail.com>
2025-12-12 13:16:43 +0530

    buy subscription controller

--- server/controllers/payment.controller.js
+++ server/controllers/payment.controller.js
@@ -1,3 +1,7 @@
+import User from "../models/user.model.js";
+import { razorpay } from "../server.js";
+import AppError from "../utils/error.utils";
+
 //Get razorpay key
 export const getRazorpayApiKey = async (req, res, next) => {
 
@@ -10,6 +14,33 @@ export const getRazorpayApiKey = async (req, res, next) => {
 }
 
 export const buySubscription = async (req, res, next) => {
+
+    const { id } = req.user;
+    const user = await User.findById(id);
+
+    if(!user) {
+        return next(new AppError('Unauthorized! Please login.'), 400)
+    }
+
+    if(user.role === 'ADMIN') {
+        return next(new AppError('Admin cannot purchase a subscription.', 400))
+    }
+
+    const subscription = await razorpay.subscriptions.create({
+        plan_id: process.env.RAZORPAY_PLAN_ID,
+        customer_notify: 1
+    });
+
+    user.subscription.id = subscription.id;
+    user.subscription.status = subscription.status;
+
+    await user.save();
+
+    res.status(200).json({
+        success: true,
+        message: 'Subscribed Successfully',
+        subscription_id: subscription.id
+    })
 
 }
 
